229abe6d9211dc62c61e0a79b770142a680eeeb5
Jeremiah <jeremiahmarcos27@outlook.com>
2022-09-27 14:33:44 -0500

    player name, info and image extracted using uipath and stored in players_info.xlsx file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [randomedPlayerNum, setRandomedPlayerNum] = useState(237)
+  const [randomedPlayer, setRandomedPlayer] = useState(null)
+
+
+  useEffect(() => {
+    fetch(`https://balldontlie.io/api/v1/players/237`, {
+      method: "GET",
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          console.log(user)
+        });
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
+        <h1>Hello</h1>
+        <h1>{randomedPlayerNum}</h1>
+        <p>{randomedPlayer}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
