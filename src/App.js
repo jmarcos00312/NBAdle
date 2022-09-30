da2b49014f77639e3412879d171b5ea7b7b506f4
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-09-30 14:09:20 -0500

    added component and mock data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
+import Keyboard from './keyboard.js'
+
 
 function App() {
   const [randomedPlayerNum, setRandomedPlayerNum] = useState(237)
   const [randomedPlayer, setRandomedPlayer] = useState(null)
 
 
-  useEffect(() => {
-    fetch(`https://balldontlie.io/api/v1/players/237`, {
-      method: "GET",
-      mode: 'cors',
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }).then((res) => {
-      if (res.ok) {
-        res.json().then((user) => {
-          console.log(user)
-        });
-      }
-    })
-  }, [])
+  // useEffect(() => {
+  //   fetch(`https://balldontlie.io/api/v1/players/237`, {
+  //     method: "GET",
+  //     mode: 'cors',
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     }
+  //   }).then((res) => {
+  //     if (res.ok) {
+  //       res.json().then((user) => {
+  //         console.log(user)
+  //       });
+  //     }
+  //   })
+  // }, [])
+
+
+  const players = {
+    name: 'Steven Adams',
+    team: "MEM",
+    jersey: "4",
+    position: "C",
+    height: "6-11",
+    weight: "265lbs",
+    college: "Pittsburgh",
+    country: "New Zealand"
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
-        <h1>{randomedPlayerNum}</h1>
+        <h1>{players.name}</h1>
         <p>{randomedPlayer}</p>
+        <Keyboard />
       </header>
     </div>
   );
---
+++ src/keyboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Keyboard = () => {
+  return (
+    <div>Keyboard</div>
+  )
+}
+
+
+export default Keyboard
