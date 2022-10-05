d47406a366238a11c5d96ced4aecdf8b4f4ab0ae
Jeremiah <jeremiahmarcos27@outlook.com>
2022-10-05 15:17:44 -0500

    added the wrong player guess and add the input box

--- src/components/guess.js
+++ src/components/guess.js
@@ -6,8 +6,12 @@ import React, { useState } from 'react'
 
 function Guess({ players }) {
   const [wrongLetters, setWrongLetters] = useState([])
+
   const [correctLetters, setCorrectLetters] = useState([])
-  const [guess, setGuess] = useState([])
+  const [inputValue, setInputValue] = useState("")
+  const [guessCounter, setGuessCounter] = useState(1)
+  const [userGuess, setUserGuess] = useState([])
+  // const userGuess = []
 
   // let gameStart = <div classname="letters-blanks"> </div>
   let gameStart = Array.from(players.name).map(e => {
@@ -16,13 +20,58 @@ function Guess({ players }) {
 
   // let gameStart = <div className="letters-blanks">{players.name.replace(/[a-zA-Z]/, '_').toUpperCase()}</div>
 
+  const hadleSubmit = (e) => {
+    e.preventDefault()
+    setInputValue(e.target.value)
+    if (players.name.toLowerCase().trim() === inputValue.toLowerCase().trim()) {
+      console.log("win")
+    }
+    else {
+      setUserGuess([...userGuess, inputValue])
+    }
+    setInputValue("")
+  }
+  console.log(userGuess)
 
+
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const checkIfInputMatch = () => {
+    if (inputValue.trim() === players.name) {
+      setCorrectLetters([...correctLetters, inputValue])
+    }
+    else {
+      setWrongLetters([...wrongLetters, inputValue])
+    }
+    setInputValue("")
+  }
 
   return (
     <div>
       <img src={players.img_url} id="img_url" alt="Profile pic" />
-      <p id="wrongLetters">{wrongLetters}</p>
+      <div className="wrongLettersDiv">
+
+        <p id="eachWrongGuesses">{userGuess.map(e => {
+          return <p>{e}</p>
+        })}</p>
+      </div>
       <h1 className="toBlank">{gameStart}</h1>
+      <form onSubmit={hadleSubmit}>
+        <input
+          id="userInputLetters"
+          type="text"
+          // value={inputValue}
+          onChange={(e) => handleChange(e)}
+        // onKeyPress={e => handleInputValue(e)}
+        />
+        <input
+          type="submit"
+          name="submit"
+        />
+      </form>
       guess
     </div>
   )
